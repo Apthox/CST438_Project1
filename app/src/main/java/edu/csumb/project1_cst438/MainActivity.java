fdf99aa03e17c7007704f3793cc27128fadb11c7
EddySigma <juangarcia@csumb.edu>
2020-02-20 10:43:04 -0800

    made some changes to my files to make it easier to merge with master

--- app/src/main/java/edu/csumb/project1_cst438/MainActivity.java
+++ app/src/main/java/edu/csumb/project1_cst438/MainActivity.java
@@ -1,39 +1,69 @@
 package edu.csumb.project1_cst438;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import edu.csumb.project1_cst438.Model.ACT_Initial_Course_Display;
+import edu.csumb.project1_cst438.Model.Course;
+import edu.csumb.project1_cst438.Model.CourseDao;
+
+import edu.csumb.project1_cst438.Model.AppRoom;
 
 public class MainActivity extends AppCompatActivity {
-    private final String TAG = "MainActivity";
+
+    static public String username;
+    static public boolean signedIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // make button that takes you to Create edu.csumb.project1_cst438.Model.Assignment Activity
-        // This section should be deleted later
+        MainActivity.username = "";
+        MainActivity.signedIn = false;
 
-        Button goToCreateAssigBtn = (Button) findViewById(R.id.go_to_create_assignment_activity_btn);
-        goToCreateAssigBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, CreateAssignmentActivity.class));
-            }
-        });
+        // loads data and creates singleton instance
+        AppRoom.getAppRoom(MainActivity.this).loadData(this);
 
-        Button goToMultiAssignmentDisplayBtn = (Button) findViewById(R.id.got_to_multi_assignment_display_btn);
-        goToMultiAssignmentDisplayBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i(TAG, "This runs on button press");
-                startActivity(new Intent(MainActivity.this, MultiAssignmentDisplay.class));
-            }
-        });
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+
+
+
+    }
+
+    public static AlertDialog createAlertDialog(Context context, String title, String text) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+
+        // set title
+        alertDialogBuilder.setTitle(title);
+
+        // set dialog message
+        alertDialogBuilder
+                .setMessage(text)
+                .setCancelable(true)
+                .setNegativeButton("Close",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        return alertDialogBuilder.create();
+    }
+
+    public static void logout() {
+        MainActivity.username = "";
+        MainActivity.signedIn = false;
     }
 }
--- app/src/main/java/edu/csumb/project1_cst438/Model/AppRoom.java
+++ app/src/main/java/edu/csumb/project1_cst438/Model/AppRoom.java
@@ -1,16 +1,65 @@
 package edu.csumb.project1_cst438.Model;
 
 import android.content.Context;
+import android.util.Log;
 
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import java.util.List;
+
 @Database(entities = {User.class, Assignment.class}, version = 1)
 public abstract class AppRoom extends RoomDatabase {
+    private static AppRoom instance;
+
     public static final String dbName = "db-studentGradeTracker";
-    public static final String ASSIGNMENTS_TABLE = "Assignment_Table";
 
     public abstract UserDao userDao();
     public abstract AssignmentDao assignmentDao();
+    public abstract CourseDao getCourseDao();
+
+    public static final String ASSIGNMENTS_TABLE = "Assignment_Table";
+    public static final String COURSE_TABLE="course";
+
+    public static AppRoom getAppRoom(final Context context){
+
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                    AppRoom.class,
+                    "FlightDB") // database name
+                    .fallbackToDestructiveMigration()
+                    .allowMainThreadQueries()  // temporary for now
+                    .build();
+        }
+        return instance;
+    }
+
+    public void loadData(Context context){
+
+        // if user table is empty, then load data for users and flights
+        List<User> user_list = AppRoom.getAppRoom(context).userDao().getAllUsers();
+        if (user_list.size() == 0) {
+            Log.d("FlightRoom", "loading data ");
+            loadUsers(context);
+        } else {
+            Log.d("FlightRoom", "data retained!");
+        }
+    }
+
+    private void loadUsers(Context context) {
+        UserDao dao = getAppRoom(context).userDao();
+
+        User alice = new User("A@lice5", "@cSit100", "Alice", "Smith");
+        User brian = new User("$BriAn7","123aBc##", "Brain", "Bob");
+        User chris = new User("!chriS12!", "CHrIS12!!", "Chris", "Sanchez");
+        User kevin = new User("kevin1", "guzman1", "Kevin", "Guzman");
+        dao.insert(alice);
+        dao.insert(brian);
+        dao.insert(chris);
+        dao.insert(kevin);
+        Log.d("FlightRoom", "4 users added to database");
+    }
+
+
 }
